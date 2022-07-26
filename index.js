288dd14552875721a621985267268862c4a97436
AmanGupta7883 <87435468+AmanGupta7883@users.noreply.github.com>
2022-07-26 23:00:29 +0530

    chatbox_update

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 comingsoon=()=>{
     alert('Coming Soon...');
 }
+chbo=()=>{
+    // document.getElementById('albox').style.innerHTML="Hi "+ $(name)+", Welcome to Skill Resource! To get any type of help click on the question.";
+    document.getElementById('chatbox').style.display='block';
+}
+cl=()=>{
+    document.getElementById('chatbox').style.display='none';
+    document.getElementById('chatans').style.innerHTML="";
+}
+q1=()=>{
+    document.getElementById('chatans').innerHTML='We had made most of the courses free. Which will help you to boost your carrer.';
+    document.getElementById('chatans').style.display='block';
+    document.getElementById('chatans').style.display='flex';
+}
+q2=()=>{
+    document.getElementById('chatans').innerHTML='<a href="mailto:techytrigger@gmail.com">Mail Here</a>';
+    document.getElementById('chatans').style.display='block';
+}
+q3=()=>{
+    document.getElementById('chatans').innerHTML='<a href="mailto:techytrigger@gmail.com">Mail Us</a> or <a href="tel:5012456985">Contact Here</a>';
+    document.getElementById('chatans').style.display='block';
+}
+q4=()=>{
+    document.getElementById('chatans').innerHTML='Yes you will get the certificate of Internship which you can also display in resume.';
+    document.getElementById('chatans').style.display='block';
+}
+cmore=()=>{
+    document.getElementById('chatQ').style.height='80%';
+    document.getElementById('sdown').style.display='none';
+}
